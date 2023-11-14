72478bc0aa6318de8be16603730699e70288b06f
solsolsole <dmsthf917@naver.com>
2023-11-14 17:46:41 +0900

    Use List

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -6,14 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class ArticleController {
-    private Article[] articles = new Article[3];
-    private int articlesLastIndex = -1; // 배열의 길이는 3이지만 아직 아무런 값을 안 넣었을때 다시 0으로 초기화 하기 위함
-                                        // 즉 -1을 초기값으로 설정하여 첫번째 요소를 articles[0] 부터 넣기 위함이다.
+
+    private List<Article> articles = new ArrayList<>();
 
     @GetMapping("/article/write")
     String showWrite() {
@@ -26,14 +27,13 @@ public class ArticleController {
             String title,
             String body
     ) {
-        Article article = new Article(articlesLastIndex + 2, title, body);
+        Article article = new Article(articles.size() + 1, title, body);
 
         Map<String, Object> rs = new HashMap<>();
         rs.put("msg","%d번 게시물이 작성되었습니다.".formatted(article.getId()));
         rs.put("data", article);
 
-        articles[++articlesLastIndex] = article;
-
+        articles.add(article);
 
         return rs;
     }
@@ -41,12 +41,12 @@ public class ArticleController {
     @GetMapping("/article/getLastArticle")
     @ResponseBody
     Article getLastArticle() {
-        return articles[articlesLastIndex];
+        return articles.getLast();
     }
 
     @GetMapping("/article/getArticles")
     @ResponseBody
-    Article[] getArticles() {
+    List<Article> getArticles() {
         return articles;
     }
 
